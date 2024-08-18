d874177fce1f3fedbadeb22b40f5cb1ff77457cb
mike7051141 <mike705114@gmail.com>
2024-08-18 14:14:22 +0900

    fix Update Team
    - 권한에 따른 팀 수정
    - 팀 정보 출력 -> 팀상세 페이지에서 필요한 정보들 가져오기

--- src/main/java/com/springboot/publicplace/controller/TeamController.java
+++ src/main/java/com/springboot/publicplace/controller/TeamController.java
@@ -2,14 +2,12 @@ package com.springboot.publicplace.controller;
 
 import com.springboot.publicplace.dto.ResultDto;
 import com.springboot.publicplace.dto.request.TeamRequestDto;
+import com.springboot.publicplace.dto.response.TeamResponseDto;
 import com.springboot.publicplace.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,6 +21,24 @@ public class TeamController {
     public ResponseEntity<ResultDto> createTeam(@RequestBody TeamRequestDto requestDto, HttpServletRequest servletRequest) {
         ResultDto resultDto = teamService.createTeam(servletRequest, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(resultDto);
+    }
+
+    @PutMapping("/updateTeam")
+    public ResponseEntity<ResultDto> updateTeam(@RequestParam Long teamId, HttpServletRequest servletRequest,@RequestBody TeamRequestDto requestDto) {
+        ResultDto resultDto = teamService.updateTeam(teamId, servletRequest, requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(resultDto);
+    }
+
+    @PostMapping("/joinTeam/{teamId}")
+    public ResponseEntity<ResultDto> joinTeam(@PathVariable Long teamId, HttpServletRequest servletRequest) {
+        ResultDto resultDto = teamService.joinTeam(teamId, servletRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(resultDto);
+    }
+
+    @GetMapping("/getTeam/{teamId}")
+    public ResponseEntity<TeamResponseDto> getTeam(@PathVariable Long teamId) {
+        TeamResponseDto teamInfo = teamService.getTeamInfo(teamId);
+        return ResponseEntity.ok(teamInfo);
     }
 
 }
--- src/main/java/com/springboot/publicplace/service/TeamService.java
+++ src/main/java/com/springboot/publicplace/service/TeamService.java
@@ -2,9 +2,14 @@ package com.springboot.publicplace.service;
 
 import com.springboot.publicplace.dto.ResultDto;
 import com.springboot.publicplace.dto.request.TeamRequestDto;
+import com.springboot.publicplace.dto.response.TeamResponseDto;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface TeamService{
     ResultDto createTeam(HttpServletRequest request, TeamRequestDto teamRequestDto);
+    ResultDto updateTeam(Long teamId, HttpServletRequest request, TeamRequestDto teamRequestDto);
+    ResultDto joinTeam(Long teamId, HttpServletRequest servletRequest);
+    TeamResponseDto getTeamInfo(Long teamId);
+
 }
