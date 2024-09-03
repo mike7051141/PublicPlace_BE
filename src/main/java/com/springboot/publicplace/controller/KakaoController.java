3645bb9be2fcb5df7ffc16f740dabd70f17fe348
mike7051141 <mike705114@gmail.com>
2024-09-03 17:11:03 +0900

    Update KakaoController.java

--- src/main/java/com/springboot/publicplace/controller/KakaoController.java
+++ src/main/java/com/springboot/publicplace/controller/KakaoController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/v1/kakao")
@@ -30,6 +31,7 @@ public class KakaoController {
     }
 
     @GetMapping("/callback")
+    @ResponseBody
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
         return kakaoService.getKaKaoUserInfo(code);
     }
