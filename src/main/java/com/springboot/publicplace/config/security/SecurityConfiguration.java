ec6069079f4d046d3b2fc160121875fc98793fd7
mike7051141 <mike705114@gmail.com>
2024-09-05 23:32:31 +0900

    Rollback

--- src/main/java/com/springboot/publicplace/config/security/SecurityConfiguration.java
+++ src/main/java/com/springboot/publicplace/config/security/SecurityConfiguration.java
@@ -27,27 +27,34 @@ public class SecurityConfiguration {
     }
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        httpSecurity.httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
+
+                .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
+
+                .sessionManagement()
+                .sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
+
                 .and()
-                .authorizeRequests()
-                .antMatchers("/sign-api/sign-in", "/sign-api/sign-up", "/kakao/callback",
-                        "/sign-api/exception", "/api/files/**").permitAll()
+                .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/sign-api/sign-in", "/sign-api/sign-up",("/kakao/callback"),
+                        "/sign-api/exception", "/api/files/**").permitAll() // 가입 및 로그인 주소는 허용
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
-                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll() // Swagger 관련 경로는 인증 없이 허용
-                .anyRequest().hasRole("ADMIN")
+
+                .antMatchers("**exception**").permitAll()
+
+                .anyRequest().hasRole("ADMIN") // 나머지 요청은 인증된 ADMIN만 접근 가능
+
                 .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class);
-
+                        UsernamePasswordAuthenticationFilter.class); // JWT Token 필터를 id/password 인증 필터 이전에 추가
         return httpSecurity.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -57,6 +64,7 @@ public class SecurityConfiguration {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/v2/api-docs",
+                "/index.html",
                 "/swagger-resources/**",
                 "/swagger-ui.html",
                 "/webjars/**",
@@ -65,5 +73,4 @@ public class SecurityConfiguration {
                 "/kakao/**",
                 "/kakao_login_large_narrow.png");
     }
-
 }
