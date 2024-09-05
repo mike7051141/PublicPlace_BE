49bafaa3d6e6e19714bb60f6e77fccc5260eb4ac
mike7051141 <mike705114@gmail.com>
2024-09-05 23:10:43 +0900

    fix Cors ....

--- src/main/java/com/springboot/publicplace/config/WebConfig.java
+++ src/main/java/com/springboot/publicplace/config/WebConfig.java
@@ -1,17 +1,26 @@
 package com.springboot.publicplace.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 허용할 도메인 설정
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
+        config.setAllowedHeaders(Arrays.asList("*")); // 허용할 헤더 설정
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
--- src/main/java/com/springboot/publicplace/config/security/SecurityConfiguration.java
+++ src/main/java/com/springboot/publicplace/config/security/SecurityConfiguration.java
@@ -27,34 +27,29 @@ public class SecurityConfiguration {
     }
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
-
-                .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
-
-                .sessionManagement()
-                .sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
-
+        httpSecurity
+                .cors() // CORS 허용
                 .and()
-                .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
-                .antMatchers("/sign-api/sign-in", "/sign-api/sign-up",("/kakao/callback"),
-                        "/sign-api/exception", "/api/files/**").permitAll() // 가입 및 로그인 주소는 허용
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/sign-api/sign-in", "/sign-api/sign-up", "/kakao/callback",
+                        "/sign-api/exception", "/api/files/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
-
-                .antMatchers("**exception**").permitAll()
-
-                .anyRequest().hasRole("ADMIN") // 나머지 요청은 인증된 ADMIN만 접근 가능
-
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll() // Swagger 관련 경로는 인증 없이 허용
+                .anyRequest().hasRole("ADMIN")
                 .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class); // JWT Token 필터를 id/password 인증 필터 이전에 추가
+                        UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -64,7 +59,6 @@ public class SecurityConfiguration {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/v2/api-docs",
-                "/index.html",
                 "/swagger-resources/**",
                 "/swagger-ui.html",
                 "/webjars/**",
@@ -73,4 +67,5 @@ public class SecurityConfiguration {
                 "/kakao/**",
                 "/kakao_login_large_narrow.png");
     }
+
 }
