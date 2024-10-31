9b57d04f2084aa8e7078e3807e0287e88682646c
mike7051141 <mike705114@gmail.com>
2024-10-31 14:46:50 +0900

    Update WebConfig.java

--- src/main/java/com/springboot/publicplace/config/WebConfig.java
+++ src/main/java/com/springboot/publicplace/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://3.36.221.111")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
