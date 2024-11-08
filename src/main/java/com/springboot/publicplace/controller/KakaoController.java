f107d67325eb57bb4a5e1bac4294fa2758381861
mike7051141 <mike705114@gmail.com>
2024-11-08 23:50:43 +0900

    Kakao Test

--- src/main/java/com/springboot/publicplace/controller/KakaoController.java
+++ src/main/java/com/springboot/publicplace/controller/KakaoController.java
@@ -2,6 +2,9 @@ package com.springboot.publicplace.controller;
 
 import com.springboot.publicplace.service.KakaoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
-
+@Slf4j
 @Controller
 @RequestMapping("/api/v1/kakao")
 @RequiredArgsConstructor
@@ -31,11 +34,25 @@ public class KakaoController {
     @Autowired
     private HttpServletRequest request;
 
+
     @GetMapping("/page")
     public ResponseEntity<String> loginPage() {
         // 요청의 호스트 정보를 읽어 동적으로 redirect_url 구성
-        String host = request.getRequestURL().toString().replace(request.getRequestURI(), "");
-        String redirectUrl = host.replace(":8080", ":3000") + "/api/v1/kakao/callback";
+//        String host = request.getRequestURL().toString().replace(request.getRequestURI(), "");
+//        String redirectUrl = host.replace(":8080", ":3000") + "/api/v1/kakao/callback";
+
+        String serverName = request.getServerName();
+        String redirectUrl;
+        log.info("Server Name: {}", serverName);
+        // 서버 이름에 따라 리다이렉트 URL 설정
+        if (serverName.equals("publicplace.site")) {
+            redirectUrl = "http://publicplace.site/api/v1/kakao/callback";
+        } else if (serverName.equals("3.36.221.111")) {
+            redirectUrl = "http://3.36.221.111/api/v1/kakao/callback";
+        } else {
+            // 기본 리다이렉트 URL 설정
+            redirectUrl = "http://publicplace.site/api/v1/kakao/callback";
+        }
 
         // 카카오 인증 URL 생성
         String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + client_id + "&redirect_uri=" + redirectUrl;
